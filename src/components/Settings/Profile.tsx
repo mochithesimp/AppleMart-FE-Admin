78080367d3476e76f8bc406d3ff8bc3f28bdb81d
HCM24_CPL_NET_14_NguyenThanhNhan <nhanntse172412@fpt.edu.vn>
2025-03-29 08:54:47 +0700

    Fix profile admin

--- src/components/Settings/Profile.tsx
+++ src/components/Settings/Profile.tsx
@@ -3,12 +3,42 @@ import SettingSection from "./SettingSection";
 import "./Profile.css";
 import { Link } from "react-router-dom";
 import noface from "../../assets/NoFace.jpg";
+import { useEffect, useState } from "react";
+import { getUserIdFromToken } from "../../utils/jwtHelper";
+import { getUserId } from "../../apiServices/UserServices/userServices";
 
+export interface IUser {
+  role: string;
+  name: string;
+  email: string;
+}
 const Profile = () => {
   const handleLogout = () => {
     localStorage.removeItem("token");
     localStorage.removeItem("refreshToken");
   };
+
+  const token = localStorage.getItem("token");
+  const [user, setUser] = useState({} as IUser);
+
+  useEffect(() => {
+    const fetchUser = async () => {
+      try {
+        if (!token) {
+          console.error("Token not found");
+          return;
+        }
+        const userIdFromToken = getUserIdFromToken(token) || "";
+        const userData = await getUserId(userIdFromToken);
+        setUser(userData);
+      } catch (error) {
+        console.error("Error fetching user", error);
+        throw new Error("User not found");
+      }
+    };
+
+    fetchUser();
+  }, [token]);
 
   return (
     <SettingSection icon={User} title="User Profile">
@@ -19,8 +49,8 @@ const Profile = () => {
           className="profile-avatar"
         />
         <div className="profile-info">
-          <h3 className="profile-name">Admin</h3>
-          <p className="profile-email">admin@example.com</p>
+          <h3 className="profile-name">{user.name}</h3>
+          <p className="profile-email">{user.email}</p>
         </div>
       </div>
 
